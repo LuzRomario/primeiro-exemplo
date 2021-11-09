7d2947816285416a00854000edd5ff4c1d0cefae
LuzRomario <dangelo.romario@gmail.com>
2021-11-09 14:59:11 -0500

    ok

--- crypto_wallt/config/routes.rb
+++ crypto_wallt/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'welcome#index'
   resources :mining_types
-  get 'welcome/index'
   resources :coins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
