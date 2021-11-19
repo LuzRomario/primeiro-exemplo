2b1ed76d2e6ea391583422a874b03a4f353adc79
LuzRomario <dangelo.romario@gmail.com>
2021-11-19 08:46:11 -0500

    ok

--- crypto_wallt/app/controllers/application_controller.rb
+++
@@ -1,8 +0,0 @@
-class ApplicationController < ActionController::Base
-  before_action :set_locale
-
-  def set_locale
-    cookies[:locale] = params[:locale] if params[:locale]
-    I18n.locale = cookies[:locale] if cookies[:locale] && (I18n.locale != cookies[:locale])
-  end
-end
